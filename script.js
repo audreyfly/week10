5996acd8ef4faec7e9ffb10445df84efc4b38cb2
audreyfly <95317631+audreyfly@users.noreply.github.com>
2021-12-18 13:24:53 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+   
+function  go(){
+	let subjects =["english","irish","maths","chemistry"];
+	let results=[""]
+ results=parseInt(prompt("Enter your subjects"));
+   {
+     alert (subjects[results-1]);
+} 
+}
+
+
+
+
+  
+  
+
+		
+		
+	
+	
+    
+
+	
+
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+  
